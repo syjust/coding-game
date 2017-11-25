e0bd4bbf581f32de9bb8d9e12f26c142c710b9c8
Sylvain JUST <sylvain.just@i-sloth.com>
2017-11-25 15:03:55 +0100

    add samples & doc from https://www.isograd.com/FR/solutionconcours.php
    branch:master

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # coding-game
+
+Exercices extracted from https://www.isograd.com/FR/solutionconcours.php
+Battle Dev RegionsJob -Novembre 2017
---
+++ exo/exo.php
@@ -0,0 +1,24 @@
+<?php
+/*******
+ * default base for PHP to run in https://demo.isograd.com/runtest/runtest.php
+ *
+ * Read input from STDIN
+ * Use echo or print to output your result, use the PHP_EOL constant at the end of each result line.
+ * Use:
+ *      local_echo( $variable ); 
+ * to display simple variables in a dedicated area.
+ * 
+ * Use:
+ *      local_print_r( $array ); 
+ * to display arrays in a dedicated area.
+ * ***/
+
+do{
+    $f = stream_get_line(STDIN, 10000, PHP_EOL);
+    if($f!==false){
+        $input[] = $f;
+    }
+}while($f!==false);
+
+/* Vous pouvez aussi effectuer votre traitement ici après avoir lu toutes les données */
+?>
