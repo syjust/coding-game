90069539ba5507beb999214ee51e2eabccab7aa1
Sylvain Just <sy.just@i-sloth.com>
2017-12-28 18:56:29 +0100

    04 adn input stream

--- exo/04-adn.php
+++ exo/04-adn.php
@@ -1,8 +1,26 @@
 <?php
-/* TODO get input from stdin */
-$input = [];
+/*******
+ * default base for PHP to run in https://demo.isograd.com/runtest/runtest.php
+ *
+ * Read input from STDIN
+ * Use echo or print to output your result, use the PHP_EOL constant at the end of each result line.
+ * Use:
+ *      local_echo( $variable );
+ * to display simple variables in a dedicated area.
+ *
+ * Use:
+ *      local_print_r( $array );
+ * to display arrays in a dedicated area.
+ * ***/
 
-// first  : extract line_count (predict we have 8 lines
+do{
+    $f = stream_get_line(STDIN, 10000, PHP_EOL);
+    if($f!==false){
+        $input[] = $f;
+    }
+}while($f!==false);
+
+// first  : extract line_count (predict we have 8 lines)
 $line_count = array_shift($input);
 
 // second : generate 8 fragments array of array
