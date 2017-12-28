8132be2c52ce7477a53595e93931cdaf19ca4de4
Sylvain Just <sy.just@i-sloth.com>
2017-12-28 19:02:07 +0100

    04 adn implement indexmanager->next + adnparser

--- exo/04-adn.php
+++ exo/04-adn.php
@@ -31,14 +31,113 @@ for ($i = 0 ; $i < $line_count ; $i++) {
 
 // third  : generate all framgents suites with index manager
 $indexManager = new IndexManager($line_count);
-$possiblesAdn = [];
+$adnParser = new AdnParser($input);
 do {
   if ($possibleAdn = $indexManager->extractCombination($fragments)) {
-    $possiblesAdn[] = $possibleAdn;
+    if ($adnParser->match($possibleAdn)) {
+      print $adnParser->format($possibleAdn)."\n";
+      exit(0);
+    }
+  }
+  $i++;
+
+/**
+ * AdnParser
+ */
+class AdnParser {
+  private $part_length = 0;
+  private $length = 0;
+  /**
+   * define size of one adn part
+   * & store parts of adn for future formating
+   * @param array $input as fragments given
+   */
+  public function __construct($input) {
+    $this->length      = strlen(implode($input));
+    $this->part_length = $this->length/2;
+  }
+
+  /**
+   * verify that an array representing combination (possible adn) is well formated
+   * - firstPart & secondPart have the good size
+   * - firstPart pattern match secondPart pattern
+   * @param array $combination as possibleAdn definition
+   */
+  public function match($combination) {
+    $string = implode($combination);
+    if ($this->length == strlen($string)) {
+      list($firstPart, $secondPart) = $this->getParts($combination);
+      if(strlen($firstPart) == $this->part_length && strlen($secondPart) == $this->part_length) {
+        return $this->matchAdn($firstPart, $secondPart);
+      }
+    }
+    return false;
+  }
+
+  /**
+   * @param string $firstPart
+   * @param string $secondPart
+   * @return bool
+   */
+  private function matchAdn($fistPart, $secondPart) {
+    foreach(str_split($firstPart) as $idx => $firstAtome) {
+      $secondAtome = $secondPart[$idx];
+      if (!$this->matchAtome($firstAtome, $secondAtom)) {
+        return false;
+      }
+    }
+    return true;
   }
 } while ($indexManager->next());
 
+  /**
+   * @param char $first
+   * @param char $second
+   * @return bool
+   */
+  private function matchAtome($first, $second) {
+    switch ($first) {
+      case "A" : return ($second == "T");
+      case "T" : return ($second == "A");
+      case "C" : return ($second == "G");
+      case "G" : return ($second == "C");
+    }
+    return false;
+  }
 
+  /**
+   * assume combination is well formed adn
+   * @param array $combination as possibleAdn definition
+   * @return string as formated ADN
+   */
+  public function format($combination) {
+    $fp = [];
+    $sp = [];
+    $fistPart = "";
+    do {
+      $part = array_shift($combination);
+      $firstPart .= $part;
+      $fp[] = $part;
+    } while (strlen($firstPart) < $this->part_length);
+    $adn = implode($fp)."#".implode($combination);
+  }
+
+  /**
+   * @param array $combination
+   * @return array containing first & second part (2 parts in one array)
+   */
+  private function getParts($combination) {
+    $firstPart = "";
+    do {
+      $part = array_shift($combination);
+      $firstPart .= $part;
+    } while (strlen($firstPart) < $this->part_length);
+    return [
+      $firstPart,
+      implode($combination)
+    ];
+  }
+}
 
 /**
  * IndexManager will help us to increment arrays indexes
@@ -63,13 +162,34 @@ class IndexManager {
    * @return bool true while there is a next index, false otherwise
    */
   public function next() {
+    $ret = true;
     for ($i = $this->colCount ; $i > 0 ; $i--) {
       /* increment here col by col and reinit when previous col is incremented */
+      if ($this->indexes[$i] == $this->maxIndex) {
+        if ($i == 0) {
+          $ret = false;
+          break;
+        } else {
+          $this->indexes[$i] = 0;
+        }
+      } else {
+        $this->indexes[$i]++;
+        $ret = true;
+        break;
+      }
     }
-    return false;
+    return $ret;
   }
 
+  /**
+   * @return array as combination in array of array for current index state
+   */
   public function extractCombination($arrayOfArray) {
+    $combination = [];
+    foreach($arrayOfArray as $idx => $array) {
+      $combination[] = $array[$this->indexes[$idx]];
+    }
+    return $combination;
   }
 
 }
