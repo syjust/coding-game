106ff3473de0abcc309dc5f21f774cc60697a449
Sylvain Just <sy.just@i-sloth.com>
2017-12-29 16:13:00 +0100

    05 start pizzarama impl

--- exo/05-pizzarama.php
+++ exo/05-pizzarama.php
@@ -22,4 +22,56 @@ do{
 }while($f!==false);
 
 /* Vous pouvez aussi effectuer votre traitement ici après avoir lu toutes les données */
+$line_count = array_shift($input);
+$stations = [];
+foreach ($input as $coord) {
+  $stations[] = new Station($coord);
+}
+$i = null;
+$j = null;
+$dist = 0;
+foreach ($stations as $station) {
+  if (is_null($i)) {
+    $i = $station;
+    continue;
+  }
+  if (!is_null($j)) {
+    $i = $j;
+  }
+  $j = $station;
+  $dist += $i->dist($j);
+}
+// todo: round dist with one of following args
+// #PHP_ROUND_HALF_UP
+// #PHP_ROUND_HALF_DOWN
+// #PHP_ROUND_HALF_EVEN
+// #PHP_ROUND_HALF_ODD
+echo $dist."\n";
+
+class Station implements Comparable {
+  private $x = 0;
+  private $y = 0;
+  private $z = 0;
+
+  public function __construct($coord) {
+    list($this->x, $this->y, $this->z) = explode(" ", $coord);
+  }
+
+  public function compareTo($station) {
+    if (!$station instanceof Station) {
+      throw new Exception("cannot compare something else than a station");
+    }
+    if ($this->y > $station->y) {
+      return 1;
+    } else if ($this->y < $station->y) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }
+  // todo: implement dist with following algorithm
+  // sqrt(pow(xj-xi, 2)+pow(yj-yi, 2)+pow(zj-zi, 2));
+  public function dist($station) {
+  }
+}
 ?>
