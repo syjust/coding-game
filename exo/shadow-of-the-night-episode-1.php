cb24107bd473d23aa89b1561161a1e574a8fdfb6
Sylvain JUST <sylvain.just@i-sloth.com>
2018-03-08 09:58:02 +0100

    shadow of the night 1 : refactor grid business methods in batman
    refactor for a more comprehensive behavior of the program
    do not reinstentiate classes each loop
    TODO: rewrite (and maybe split) business rules because codes still doesn't work
    
    branch:master

--- exo/shadow-of-the-night-episode-1.php
+++ exo/shadow-of-the-night-episode-1.php
@@ -9,124 +9,131 @@ function debug($string) {
 class Batman {
     public $x;
     public $y;
-    public function __construct($x, $y) {
+    public $grid;
+    public function __construct($x, $y, Grid $grid) {
         debug("new Batman($x, $y)");
+        $this->go($x, $y);
+        $this->grid = $grid;
+    }
+    public function go($x, $y) {
         $this->x = $x;
         $this->y = $y;
     }
     public function __toString() {
         return sprintf("%d %d\n", $this->x, $this->y);
     }
+
+    /**
+     * cropGrid
+     *
+     * @param string $bombDir
+     *
+     * @author sylvain.just
+     * @date 2018-02-25
+     */
+    public function cropGrid($bombDir) {
+        debug("cropGrid($bombDir)");
+        switch($bombDir) {
+            // up 
+            case 'U'   :
+                $this->grid->crop(
+                    $this->x,
+                    $this->grid->y,
+                    1,
+                    $this->y
+                );
+            break;
+            // down
+            case 'D'   :
+                $this->grid->crop(
+                    $this->x,
+                    $this->y+1,
+                    1,
+                    $this->grid->height-$this->y
+                );
+            break;
+            // right
+            case 'R'   :
+                $this->grid->crop(
+                    $this->x+1,
+                    $this->y,
+                    $this->grid->width-$this->x,
+                    1
+                );
+            break;
+            // left
+            case 'L'   :
+                $this->grid->crop(
+                    $this->grid->x,
+                    $this->y,
+                    $this->x,
+                    1
+                );
+            break;
+            // up-right
+            case 'UR'  :
+                $this->grid->crop(
+                    $this->x,
+                    $this->y,
+                    $this->grid->width-$this->x,
+                    $this->y
+                );
+            break;
+            // down-right
+            case 'DR'  :
+                $this->grid->crop(
+                    $this->x,
+                    $this->y+1,
+                    $this->grid->width-$this->x,
+                    $this->grid->height-$this->y
+                );
+            break;
+            // down-left
+            case 'DL'  :
+                $this->grid->crop(
+                    $this->x,
+                    $this->y+1,
+                    $this->grid->width-$this->x,
+                    $this->grid->height-$this->y
+                );
+            break;
+            // up-left
+            case 'UL'  :
+                $this->grid->crop(
+                    $this->grid->x,
+                    $this->grid->y,
+                    $this->grid->width-$this->x,
+                    $this->grid->height-$this->y
+                );
+            break;
+        }
+        return $grid;
+    }
+    public function gotoNext() {
+        $x = $this->grid->width > 2
+            ? (int)$this->grid->width/2 + $this->grid->x
+            : $this->grid->width + $this->grid->x;
+        $y = $this->grid->height > 2
+            ? (int)$this->grid->height/2 + $this->grid->y
+            : $this->grid->height + $this->grid->y;
+        $this->go($x, $y);
+    }
+
 }
 class BatGrid {
     public $width  = 0;
     public $height = 0;
     public $x      = 0;
     public $y      = 0;
-    public function __construct($width, $height, $x = 0, $y = 0) {
-        debug("new BatGrid($width, $height, $x, $y)");
+    public function __construct($x, $y, $width, $height) {
+        debug("new BatGrid($x, $y, $width, $height)");
+        $this->crop($x, $y, $width, $height);
+    }
+    public function crop($x, $y, $width, $height) {
         $this->width = $width;
         $this->height = $height;
         $this->x = $x;
         $this->y = $y;
-    }
-    /**
-     * getGrid
-     *
-     * @param Batman $batman
-     * @param mixed $bombDir
-     * @return BatGrid
-     *
-     * @author sylvain.just
-     * @date 2018-02-25
-     */
-    public function getGrid(Batman $batman, $bombDir) {
-        debug("getGrid($batman, $bombDir)");
-        $grid = null;
-        switch($bombDir) {
-            // up 
-            case 'U'   :
-                $grid = new BatGrid(
-                    1,
-                    $batman->y,
-                    $batman->x,
-                    $this->y 
-                );
-            break;
-            // down
-            case 'D'   :
-                $grid = new BatGrid(
-                    1,
-                    $this->height-$batman->y,
-                    $batman->x,
-                    $batman->y+1
-                );
-            break;
-            // right
-            case 'R'   :
-                $grid = new BatGrid(
-                    $this->width-$batman->x,
-                    1,
-                    $batman->x+1,
-                    $batman->y
-                );
-            break;
-            // left
-            case 'L'   :
-                $grid = new BatGrid(
-                    $batman->x,
-                    1,
-                    $this->x,
-                    $batman->y
-                );
-            break;
-            // up-right
-            case 'UR'  :
-                $grid = new BatGrid(
-                    $this->width-$batman->x,
-                    $batman->y,
-                    $batman->x,
-                    $batman->y
-                );
-            break;
-            // down-right
-            case 'DR'  :
-                $grid = new BatGrid(
-                    $this->width-$batman->x,
-                    $this->height-$batman->y,
-                    $batman->x,
-                    $batman->y+1
-                );
-            break;
-            // down-left
-            case 'DL'  :
-                $grid = new BatGrid(
-                    $this->width-$batman->x,
-                    $this->height-$batman->y,
-                    $batman->x,
-                    $batman->y+1
-                );
-            break;
-            // up-left
-            case 'UL'  :
-                $grid = new BatGrid(
-                    $this->width-$batman->x,
-                    $this->height-$batman->y,
-                    $this->x,
-                    $this->y
-                );
-            break;
-        }
-        return $grid;
-    }
-    public function getNextBatman() {
-        $x = $this->width > 2
-            ? (int)$this->width/2 + $this->x
-            : $this->width + $this->x;
-        $y = $this->height > 2
-            ? (int)$this->height/2 + $this->y
-            : $this->height + $this->y;
-        return new Batman($x, $y);
     }
 }
 
@@ -134,7 +141,7 @@ fscanf(STDIN, "%d %d",
     $W, // width of the building.
     $H // height of the building.
 );
-$grid = new BatGrid($W, $H);
+$grid = new BatGrid(0, 0, $W, $H);
 fscanf(STDIN, "%d",
     $N // maximum number of turns before game over.
 );
@@ -142,7 +149,7 @@ fscanf(STDIN, "%d %d",
     $X0,
     $Y0
 );
-$batman = new Batman($X0, $Y0);
+$batman = new Batman($X0, $Y0, $grid);
 
 // game loop
 while (TRUE)
@@ -150,8 +157,8 @@ while (TRUE)
     fscanf(STDIN, "%s",
         $bombDir // the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
     );
-    $grid = $grid->getGrid($batman, $bombDir);
-    $batman = $grid->getNextBatman();
+    $batman->cropGrid($bombdir);
+    $batman->gotoNext($bombdir);
 
     // Write an action using echo(). DON'T FORGET THE TRAILING \n
     // To debug (equivalent to var_dump): error_log(var_export($var, true));
